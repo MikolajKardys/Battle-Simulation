0a36cdcc6b25afd1a0b8dd23b651ba4aab52460d
Dominik Sulik <72816824+MiniSek@users.noreply.github.com>
2021-06-07 18:33:01 +0200

    methods_for_u

--- src/main/scala/mapGenerator/MapGenerator.scala
+++ src/main/scala/mapGenerator/MapGenerator.scala
@@ -1,5 +1,7 @@
 package mapGenerator
 
+import utilities.Vector2D
+
 import java.util
 
 /*
@@ -7,7 +9,10 @@ import java.util
 */
 
 object MapGenerator extends App {
-  class Cell(val squareSide: Int, val x: Int, val y: Int, val isoValue: Int, val altitude: Double) {
+  class Cell(val squareSide: Int, val x: Int, val y: Int, val isoValue: Int, val altitude: Double, var denseForestIsoValue: Int) {
+    val isDenseForest: Boolean = if(MapConst.inForestIsoValue.contains(denseForestIsoValue)) true else false
+    val isSparseForest: Boolean = if(!isDenseForest && MapConst.inForestIsoValue.contains(isoValue)) true else false
+
     val yNORTH = 0
     val xNORTH: Int = squareSide/2
     val yEAST: Int = squareSide/2
@@ -88,8 +93,8 @@ object MapGenerator extends App {
     var yWEST = None: Option[Int]
     var xWEST = None: Option[Int]*/
 
-    def apply(squareSide: Int, x: Int, y: Int, isoValue: Int, altitude: Double): Cell =
-      new Cell(squareSide, x, y, isoValue, altitude)
+    def apply(squareSide: Int, x: Int, y: Int, isoValue: Int, altitude: Double, denseForestIsoValue: Int): Cell =
+      new Cell(squareSide, x, y, isoValue, altitude, denseForestIsoValue)
     /*def apply(_squareSide: Int): Unit = {
       squareSide = Some(_squareSide)
 
@@ -104,7 +109,16 @@ object MapGenerator extends App {
     }*/
   }
 
-  class Map(val height: Int, val width: Int) {
+  object MapConst {
+    val inForestIsoValue: Array[Int] = Array(3, 5, 6, 7, 9, 10, 11, 12, 13, 14 ,15)
+
+    val biomFrequency = 0.02
+    val terrainFrequency = 0.02
+    val sparseForestThresh = 0.3
+    val denseForestThresh: Double = sparseForestThresh + 0.1
+  }
+
+  class Map(val height: Int, val width: Int) extends Terrain {
     import mapGUI.MapGUI
 
     val squareSide:Int = Math.min(800/width, 800/height)
@@ -112,17 +126,20 @@ object MapGenerator extends App {
     val windowHeight:Int = squareSide * height
 
     //Cell(squareSide)
-    val perlin: PerlinNoise = PerlinNoise(128)
-    val biomFrequency = 0.02
-    val terrainFrequency = 0.02
+    val biomPerlin: PerlinNoise = PerlinNoise(128)
+    val terrainPerlin: PerlinNoise = PerlinNoise(128)
 
     //point 1 is black point 0 is white
-    val points: Seq[Vector[Int]] = Vector.tabulate(height+1, width+1)((x, y) => if(perlin.noise(x*biomFrequency, y*biomFrequency) >= 0.2) 1 else 0)
+    val points: Seq[Vector[Double]] = Vector.tabulate(height+1, width+1)((x, y) => biomPerlin.noise(x*MapConst.biomFrequency, y*MapConst.biomFrequency))
+
     val map: Seq[Vector[Cell]] = Vector.tabulate(height, width)((x, y) =>
-      Cell(squareSide, x, y, calculateIsoValue(x, y), getAltitude(perlin.noise(x*terrainFrequency, y*terrainFrequency))))
+      Cell(squareSide, x, y, calculateIsoValue(x, y, MapConst.sparseForestThresh),
+        getAltitude(terrainPerlin.noise(x*MapConst.terrainFrequency, y*MapConst.terrainFrequency)),
+        calculateIsoValue(x, y, MapConst.denseForestThresh)))
 
     val GUI = new MapGUI(windowWidth, windowHeight)
 
+    addPoints()
     addLines()
     addPolygons()
     addRectangles()
@@ -131,12 +148,23 @@ object MapGenerator extends App {
     override def toString: String =
       map.map(row => row.mkString(" ")).mkString("\n")
 
-    private def calculateIsoValue(x: Int, y: Int): Int = {
-      val a = points(x)(y)
-      val b = points(x)(y+1)
-      val c = points(x+1)(y+1)
-      val d = points(x+1)(y)
+    private def calculateIsoValue(x: Int, y: Int, thresh: Double): Int = {
+      val a = if(points(x)(y) >= thresh) 1 else 0
+      val b = if(points(x)(y+1) >= thresh) 1 else 0
+      val c = if(points(x+1)(y+1) >= thresh) 1 else 0
+      val d = if(points(x+1)(y) >= thresh) 1 else 0
       getIsoValue(a, b, c, d)
+    }
+
+    private def addPoints(): Unit = {
+      map.foreach(row => row.foreach(cell => {
+        if(cell.isDenseForest)
+          GUI.addPoint(cell.y * squareSide, cell.x * squareSide, 2)
+        else if(cell.isSparseForest)
+          GUI.addPoint(cell.y * squareSide, cell.x * squareSide, 1)
+        else
+          GUI.addPoint(cell.y * squareSide, cell.x * squareSide, 0)
+      }))
     }
 
     private def addLines(): Unit = {
@@ -168,7 +196,6 @@ object MapGenerator extends App {
             ys.foreach(y => ysJava.add(y))
 
             GUI.addPolygon(xsJava, ysJava)
-
           case _ =>
         }
       }))
@@ -176,13 +203,67 @@ object MapGenerator extends App {
 
     private def addRectangles():Unit = {
       map.foreach(row => row.foreach(cell => {
-        val xPix = cell.y*squareSide
-        val yPix = cell.x*squareSide
+        val xPix = cell.y * squareSide
+        val yPix = cell.x * squareSide
 
         GUI.addRectangle(xPix, yPix, squareSide, squareSide, cell.altitude)
       }))
     }
 
+    import utilities.TerrainType._
+    override def getTerrainType(position: Vector2D): Terrain = {
+      if(map(position.x)(position.y).isSparseForest)
+        SparseForest
+      else if(map(position.x)(position.y).isSparseForest)
+        DenseForest
+      else
+        Meadow
+    }
+
+    override def canSee(agentPosition: Vector2D, place: Vector2D): Boolean = {
+      val visionLine: Array[(Int, Int)] = getVisionLine(agentPosition.x, place.x, agentPosition.y, place.y)
+
+      var visibility: Double = 0
+      visionLine.foreach{
+        case (x: Int, y: Int) => {
+          if (map(x)(y).isDenseForest) visibility -= 2
+          else if (map(x)(y).isSparseForest) visibility -= 1
+        }
+        case _ =>
+      }
+
+      if(map(agentPosition.x)(agentPosition.y).isSparseForest && (!map(place.x)(place.y).isSparseForest && !map(place.x)(place.y).isDenseForest)) {
+        //agent is in sparse forest and place is outside of forest
+        if(visibility >= -5)
+          true
+        else
+          false
+      }
+      else if(!map(agentPosition.x)(agentPosition.y).isSparseForest && !map(agentPosition.x)(agentPosition.y).isDenseForest && (map(place.x)(place.y).isSparseForest || map(place.x)(place.y).isDenseForest)) {
+        //agent is outside of the forest and place is in the forest
+        if(visibility >= -1)
+          true
+        else
+          false
+      }
+      else {
+        //every other possibility
+        if(visibility >= -10)
+          true
+        else
+          false
+      }
+    }
+
+    override def elevation(a: Vector2D, b: Vector2D): Double = {
+      val tgTheta: Double = (map(a.x)(a.y).altitude - map(b.x)(b.y).altitude)/a.getDistance(b)
+      if(tgTheta <= 1 && tgTheta >= -1)
+        Math.exp(tgTheta)
+      else if(tgTheta > 1)
+        Math.E
+      else
+        1/Math.E
+    }
   }
 
   object Map {
@@ -204,6 +285,49 @@ object MapGenerator extends App {
     (noise+1)/2
   }
 
+  val getVisionLine: (Int, Int, Int, Int) => Array[(Int, Int)] = (xAgent, xPlace, yAgent, yPlace) => {
+    var kx: Int = 0
+    var ky: Int = 0
+    var dx: Int = 0
+    var dy: Int = 0
+    var err: Double = .0
+    var x1: Int = xAgent
+    val x2: Int = xPlace
+    var y1: Int = yAgent
+    val y2: Int = yPlace
+    var line: Array[(Int, Int)] = Array((x1, y1))
+    kx = if (x1 <= x2) 1 else -1
+    ky = if (y1 <= y2) 1 else -1
+    dx = Math.abs(x1 - x2)
+    dy = Math.abs(y1 - y2)
+
+    if(dx >= dy) {
+      err = dx.toDouble / 2
+      for(_ <- 0 until dx - 1) {
+        x1 += kx
+        err -= dy
+        if (err < 0) {
+          y1 += ky
+          err += dx
+        }
+        line = line :+ (x1, y1)
+      }
+    }
+    else {
+      err = dy.toDouble / 2
+      for (_ <- 0 until dy - 1) {
+        y1 += ky
+        err -= dx
+        if (err < 0) {
+          x1 += kx
+          err += dy
+        }
+        line = line :+ (x1, y1)
+      }
+    }
+    line = line :+ (x2, y2)
+    line
+  }
+
   val map = Map(100, 120)
-  //println(map)
 }
